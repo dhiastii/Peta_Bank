3a598f52e656880c7769ac4fd369d52e810db35f
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-11-16 23:12:12 +0700

    Create unit test file

---
+++ PetaBankTests/BankAccountTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetaBank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetaBank.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void BankAccountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BankAccountTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DebitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
